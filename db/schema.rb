6871c901830f2efa35b83779b094af78ad105451
things23 <twothreethings@gmail.com>
2015-04-24 11:00:26 +0300

    Generate model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423171537) do
+ActiveRecord::Schema.define(version: 20150424075039) do
 
   create_table "friend_requests", force: :cascade do |t|
     t.integer  "user_id"
@@ -32,6 +32,20 @@ ActiveRecord::Schema.define(version: 20150423171537) do
 
   add_index "friendships", ["friend_id"], name: "index_friendships_on_friend_id"
   add_index "friendships", ["user_id"], name: "index_friendships_on_user_id"
+
+  create_table "parties", force: :cascade do |t|
+    t.string   "name"
+    t.string   "target"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.string   "city"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.text     "info"
+    t.string   "visible"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
