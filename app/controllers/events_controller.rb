dcc76474c3929e03c9173c9a0f549bc3eab31e21
Dan K <itsdanya@gmail.com>
2015-08-14 01:13:43 +0300

    Events: update strong params filter

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -93,8 +93,7 @@ class EventsController < ApplicationController
   def events_params
     params.require(:event)
       .permit(
-        :name, :starts_at, :ends_at,
-        :city, :address, :latitude, :longitude,
+        :name, :starts_at, :city, :address, :latitude, :longitude,
         :info, :visible, :photo, :event_type_id, :user_limit,
         :min_age, :max_age, :gender
       )
