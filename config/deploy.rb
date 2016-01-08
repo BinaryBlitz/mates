2275b8aab60832d8666838b0c081a4fb6660f624
Timur Yanberdin <yanberdint@gmail.com>
2016-01-09 02:46:19 +0500

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,10 @@ after 'deploy:update_code', :roles => :app do
   run "rm -f #{current_release}/config/secrets.yml"
   run "ln -nfs #{deploy_to}/shared/config/secrets.yml #{current_release}/config/secrets.yml"
 
+  # DB
+  run "rm -f #{current_release}/config/database.yml"
+  run "ln -nfs #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
+
   # Uploads
   run "rm -f #{current_release}/public/uploads"
   run "ln -nfs #{deploy_to}/shared/public/uploads #{current_release}/public/uploads"
@@ -37,24 +41,18 @@ after 'deploy:update_code', :roles => :app do
   # Migrations
   run "cd #{current_release}; bundle exec rake db:migrate RAILS_ENV=#{rails_env}"
 
-    # Pids
+  # Pids
   run "rm -f #{current_release}/tmp/pids"
   run "ln -nfs #{deploy_to}/shared/pids #{current_release}/tmp/pids"
 end
 
-after 'deploy:update_code', 'rpush:stop', 'rpush:start'
+after 'deploy:create_symlink', 'rpush:stop', 'rpush:start'
 
-before "deploy:assets:precompile", "deploy:link_db"
 
 set :keep_releases, 3
 after "deploy:restart", "deploy:cleanup"
 
 namespace :deploy do
-  task :link_db do
-    run "rm -f #{current_release}/config/database.yml"
-    run "ln -nfs #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
-  end
-
   task :restart do
     run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
   end
@@ -68,10 +66,10 @@ end
 
 namespace :rpush do
   task :stop do
-    run "cd #{current_release}; bundle exec rpush stop --rails-env=#{rails_env}; true"
+    run "cd #{deploy_to}/current; bundle exec rpush stop --rails-env=#{rails_env}; true"
   end
 
   task :start do
-    run "cd #{current_release}; bundle exec rpush start --rails-env=#{rails_env}"
+    run "cd #{deploy_to}/current; bundle exec rpush start --rails-env=#{rails_env}"
   end
 end
