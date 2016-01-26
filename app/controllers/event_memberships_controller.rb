1dee71146978fb641bc57f5942cb303808260ab2
Dan K <itsdanya@gmail.com>
2016-01-26 23:50:00 +0300

    Fix destroy action in event memberships

--- app/controllers/event_memberships_controller.rb
+++ app/controllers/event_memberships_controller.rb
@@ -1,5 +1,6 @@
 class EventMembershipsController < ApplicationController
   before_action :set_event, only: [:index, :create]
+  before_action :set_membership, only: [:destroy]
 
   def index
     @memberships = @event.memberships
@@ -17,7 +18,6 @@ class EventMembershipsController < ApplicationController
   end
 
   def destroy
-    @membership = current_user.memberships.find(params[:id])
     authorize @membership
     @membership.destroy
     head :no_content
@@ -27,6 +27,10 @@ class EventMembershipsController < ApplicationController
 
   def set_event
     @event = Event.find(params[:event_id])
+  end
+
+  def set_membership
+    @membership = Membership.find(params[:id])
   end
 
   def membership_params
