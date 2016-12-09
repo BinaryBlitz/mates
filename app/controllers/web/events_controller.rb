9c4872a185f9d3b925f3aa54dfaa0953795bd2f2
SumLare <mrhard95@gmail.com>
2016-12-09 09:26:31 +0300

    Web: Add frontend show to events

--- app/controllers/web/events_controller.rb
+++ app/controllers/web/events_controller.rb
@@ -1,9 +1,14 @@
 class Web::EventsController < API::APIController
   skip_before_action :restrict_access
 
-  layout 'web/layouts/application'
+  layout 'web/layouts/application', except: [:frontend_show]
 
   def show
     @event = Event.find_by!(sharing_token: params[:sharing_token])
+  end
+
+  def frontend_show
+    @event = Event.find(params[:id])
+    render 'web/events/frontend_show'
   end
 end
